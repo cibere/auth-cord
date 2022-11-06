e4780509ba1df7064b13a6a62b35e081193fdccd
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-06 14:48:03 -0800

    fixed name changes

--- setup.py
+++ setup.py
@@ -9,19 +9,19 @@ with open("requirements.txt", "r") as f:
     REQUIREMENTS = f.read().splitlines()
 
 setuptools.setup(
-    name="discord-oauth",
+    name="auth_cord",
     author="cibere",
     author_email="cibere.dev@gmail.com",
-    url="https://github.com/cibere/cibere-dev.py",
+    url="https://github.com/cibere/auth_cord.py",
     project_urls={
-        "Code": "https://github.com/cibere/discord-oauth",
-        "Issue tracker": "https://github.com/cibere/discord-oauth/issues",
+        "Code": "https://github.com/cibere/auth_cord",
+        "Issue tracker": "https://github.com/cibere/auth_cord/issues",
         "Discord/Support Server": "https://discord.gg/2MRrJvP42N",
     },
     version=auth_cord.__version__,
     python_requires=">=3.8",
     install_requires=REQUIREMENTS,
-    packages=["discord_oauth"],
+    packages=["auth_cord"],
     description=auth_cord.__description__,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
