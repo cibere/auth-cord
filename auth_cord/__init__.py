f0e5f33d53a810c019590da0d8c7ad4b2588d5e9
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-06 14:49:32 -0800

    Update __init__.py

--- auth_cord/__init__.py
+++ auth_cord/__init__.py
@@ -5,7 +5,7 @@ A basic wrapper discord oauth2
 """
 
 __description__ = "A basic wrapper discord oauth2"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .authorization import Authorization
 from .client import Client
